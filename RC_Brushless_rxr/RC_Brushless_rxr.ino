75eedf0d9e99d7c8090fd778aacbbb68e555acfc
Root Said <jithinsanal1610@gmail.com>
2017-11-04 19:41:10 +0530

    Update RC_Brushless_rxr.ino

--- RC_Brushless_rxr/RC_Brushless_rxr.ino
+++ RC_Brushless_rxr/RC_Brushless_rxr.ino
@@ -1,56 +1,41 @@
-//Transmitter
-//visit www.rootsaid.com for full tutorial
+//Receiver
+//Visit www.rootsaid.com for full tutorial
 
 #include <SoftwareSerial.h>
-#include<Servo.h>         
+#include<Servo.h>          
 
 Servo esc; 
-SoftwareSerial hc12(2, 3); //RX, TX
 
-int thr, ail, ele, rud;
+String input;
+int throttle, th;
 
 void setup()
 {
 esc.attach(10);
-pinMode (A0, INPUT);
-pinMode (A1, INPUT);
-pinMode (A6, INPUT);
-pinMode (A7, INPUT);
-pinMode (A2, INPUT);
-pinMode (13, OUTPUT);
- 
 Serial.begin(9600);
 hc12.begin(9600);
+esc.write(170);
+delay(2000);
+esc.write(90);
+delay(2000);
+esc.write(140);
+delay(2000);
+esc.write(90);
+delay(2000);
 }
 
 void loop()
 {
-  ele=map(analogRead(A0), 0, 1023, 0, 180);
-  rud=map(analogRead(A1), 0, 1023, 0, 180);
-  thr=map(analogRead(A7), 0, 1023, 0, 180);
-  ail=map(analogRead(A5), 0, 1023, 0, 180);
 
-  hc12.print(thr);
-  hc12.print(",");
-  hc12.print(ail);
-  hc12.print(",");
-  hc12.print(ele);
-  hc12.print(",");
-  hc12.print(rud);
-  hc12.println("");
- 
-  Serial.print(thr);
-  Serial.print(",");
-  Serial.print(ail);
-  Serial.print(",");
-  Serial.print(ele);
-  Serial.print(",");
-  Serial.print(rud);
-  Serial.println("");
+if(Serial.available())
+  {
+  throttle = Serial.readStringUntil('\n');
+  if (input.length() > 0)
+      {
+       esc.write(throttle);
+       delay(10);      
+  }
 
+  }
 
-delay(100);  
 }
-
-
-
